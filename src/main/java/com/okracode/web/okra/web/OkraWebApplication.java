e9bd35a4de4b8b44c9b2ebe1a24744cf4a732fcb
Eric Ren <zhengxin.ren@amh-group.com>
2021-08-18 09:35:58 +0800

    init website

---
+++ src/main/java/com/okracode/web/okra/web/OkraWebApplication.java
@@ -0,0 +1,13 @@
+package com.okracode.web.okra.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkraWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkraWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okracode/web/okra/web/OkraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okracode.web.okra.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkraWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
